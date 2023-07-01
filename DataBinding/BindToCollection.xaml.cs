1388c092b2f70c10715af2b44399c30f8e589ad2
亚辉 王 <yimoo12138@outlook.com>
2023-07-01 16:54:44 +0800

    #Data Binding 1

---
+++ DataBinding/BindToCollection.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class BindToCollection : Window
+    {
+        public BindToCollection()
+        {
+            InitializeComponent();
+        }
+
+        private ObservableCollection<Product>? products;
+
+        private void cmdGetProducts_Click(object sender, RoutedEventArgs e)
+        {
+            products = products = new ObservableCollection<Product>(Product.GetProducts());
+            lstProducts.ItemsSource = products;
+        }
+
+        private void cmdDeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            products?.Remove((Product)lstProducts.SelectedItem);
+        }
+
+        private void cmdAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            products?.Add(new Product("00000", "?", "?", 0));
+        }
+
+        private bool isDirty = false;
+        private void txt_TextChanged(object sender, RoutedEventArgs e)
+        {
+            isDirty = true;
+        }
+
+        private void lstProducts_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            isDirty = false;
+        }
+    }
+}
---
+++ DataBinding/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using Bogus.DataSets;
+using Bogus.Extensions;
+
+namespace DataBinding
+{
+    class Product
+    {
+        public String ModelNumber { get; set; }
+        public String ModelName { get; set; }
+        public String Description { get; set; }
+        public decimal UnitCost { get; set; }
+
+        public Product() { }
+
+        public Product(string modelNumber, string modelName, string description, decimal unitCost)
+        {
+            ModelNumber = modelNumber;
+            ModelName = modelName;
+            Description = description;
+            UnitCost = unitCost;
+        }
+
+        public override string? ToString()
+        {
+            return $"{{ModelNumber={ModelName}, ModelName={ModelName}, Description={Description}, UnitCost={UnitCost}}}";
+        }
+
+        public static List<Product> GetProducts()
+        {
+            int productId = 1;
+            var products = new Faker<Product>()
+                .RuleFor(p => p.ModelNumber, f => (productId++).ToString())
+                .RuleFor(p => p.ModelName, f => f.Internet.UserName())
+                .RuleFor(p => p.Description, f => f.Name.JobDescriptor())
+                .RuleFor(p => p.UnitCost, f => f.Random.Decimal(0, 20))
+                .Generate(50)
+                .ToList();
+            return products;
+        }
+    }
+
+}
